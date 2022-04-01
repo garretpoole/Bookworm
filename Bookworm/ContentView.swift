53676a62b939bc89a64241dc69714f45f2502370
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-31 21:32:24 -0700

    @Binding property in struct to reflect changes in ContentView @State property

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
+struct PushButton: View{
+    let title: String
+    //@State changes internal state but wont reflect change in ContentView
+    //@Binding fixes that so "On" of "Off" will now change
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View{
+        Button(title){
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors: offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
 
 struct ContentView: View {
+    @State private var rememberMe = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            PushButton(title: "Remeber Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
