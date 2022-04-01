0d1d0b5b64ff12db06d993eaf6e95c1d46e27627
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-31 21:36:26 -0700

    TextEditor for multi-line text input

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,36 +6,17 @@
 //
 
 import SwiftUI
-struct PushButton: View{
-    let title: String
-    //@State changes internal state but wont reflect change in ContentView
-    //@Binding fixes that so "On" of "Off" will now change
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View{
-        Button(title){
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(colors: isOn ? onColors: offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 
 struct ContentView: View {
-    @State private var rememberMe = false
+    //App Storage backed by user defaults
+    @AppStorage("notes") private var notes = ""
     
     var body: some View {
-        VStack{
-            PushButton(title: "Remeber Me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+        NavigationView{
+            //use within a NavView or Form to keep editor in bounds
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
     }
 }
